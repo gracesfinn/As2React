3ba52dbb6ab2c7b6e3f113e61d61a76dc6652089
Grace S Finnerty <50175328+gracesfinn@users.noreply.github.com>
2020-08-25 21:53:14 -0500

    Read Me Added and slightly edited

--- src/pages/actorDetailsPage.js
+++ src/pages/actorDetailsPage.js
@@ -9,25 +9,12 @@ const ActorPage = props => {
   const [person] = useActor(id)
  
  
- {/* const [person, setPerson] = useState(id);
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`)
-     .then(res => res.json())
-      .then(json => {
-        console.log(json)
-        return json.results
-      })
-      .then(person =>{
-        setPerson(person);
-      });
-  }, [id]); */}
   
   return (
     <> 
     {person ? (
       <>
-          <h4>{person.name}</h4>
+         
           <div className="row">
             <div className="col-3">
             <img
@@ -39,25 +26,13 @@ const ActorPage = props => {
               : "./film-poster-placeholder.png"
           }
         />
+        
             </div>
             <div className="col-9">
-            <h4>Biography</h4>
-          <p>{person.biography}</p>
-          <ul className="list-group list-group-horizontal">
-            <li key="ruh" className="list-group-item list-group-item-dark">
-              Known For
-            </li>
-            <li key="rut" className="list-group-item ">
-              {person.known_for_department}
-            </li>
-            <li key="rdh" className="list-group-item list-group-item-dark">
-              Place of Birth
-            </li>
-            <li key="rdv" className="list-group-item ">
-              {person.place_of_birth}
-            </li></ul>
-            <ul className="list-group list-group-horizontal">
-          </ul>
+            <h4>{person.name}</h4>
+            <ActorDetails person={person}/>
+            
+            
             </div>
           </div>
     </>
